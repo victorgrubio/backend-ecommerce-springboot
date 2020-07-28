43522a22f80e24aee522ffd9e0904ab549197fa1
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2020-07-28 23:31:47 +0200

    moved

--- src/main/java/com/victorgarciarubio/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/victorgarciarubio/ecommerce/config/MyDataRestConfig.java
@@ -3,6 +3,7 @@ package com.victorgarciarubio.ecommerce.config;
 import com.victorgarciarubio.ecommerce.entity.Product;
 import com.victorgarciarubio.ecommerce.entity.ProductCategory;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
