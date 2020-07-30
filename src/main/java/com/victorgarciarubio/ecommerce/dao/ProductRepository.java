d7d9afe2916c04358c50048e1b04ffbb00fd8505
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2020-07-30 22:43:09 +0200

    Fixed bug on retrieval of information of products

--- src/main/java/com/victorgarciarubio/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/victorgarciarubio/ecommerce/dao/ProductRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
-@RepositoryRestResource(collectionResourceRel = "product", path = "products")
+@RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long>{
     
 }
