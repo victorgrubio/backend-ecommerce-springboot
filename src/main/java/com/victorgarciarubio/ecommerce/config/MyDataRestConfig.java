f2984c7571d5b9098b778737b7c25d687625aa46
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2020-08-01 20:25:35 +0200

    Dynamic category filtering. Exposed Ids of all entities

--- src/main/java/com/victorgarciarubio/ecommerce/config/MyDataRestConfig.java
+++ src/main/java/com/victorgarciarubio/ecommerce/config/MyDataRestConfig.java
@@ -1,8 +1,16 @@
 package com.victorgarciarubio.ecommerce.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+
 import com.victorgarciarubio.ecommerce.entity.Product;
 import com.victorgarciarubio.ecommerce.entity.ProductCategory;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -10,7 +18,14 @@ import org.springframework.http.HttpMethod;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
-    
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager myEntityManager){
+        entityManager = myEntityManager;
+    }
+
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config){
         HttpMethod[] theUnsupportedActions = {
@@ -27,6 +42,27 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
             .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
             .withCollectionExposure((metdata, httpMethods)-> httpMethods.disable(theUnsupportedActions));
 
+        // call internal helper method
+        exposeIds(config);
+
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config){
+        // expose entity ids
+
+        // - get a list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        for (EntityType tempEntityType : entities){
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // expose the entity ids for the array of entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
     }
 
 
