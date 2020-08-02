678f65cc58bdd531adb3dfbf8ec7f0d9d5123f5f
Victor Garcia Rubio <victorgrubiodl@gmail.com>
2020-08-02 12:43:13 +0200

    Added search by name functionality

--- src/main/java/com/victorgarciarubio/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/victorgarciarubio/ecommerce/dao/ProductRepository.java
@@ -13,4 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface ProductRepository extends JpaRepository<Product, Long>{
     
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
 }
